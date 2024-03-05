5846f47734731e5840ef5d88a5a10d7b8e9b98ba
xxaexa <akun9396@gmail.com>
2024-03-05 12:54:53 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import connectDB from "./database/connectDB";
+
 import authRoute from "./routes/authRoute";
 import noteRoute from "./routes/noteRoute";
 import userRoute from "./routes/userRoute";
+
+connectDB();
 
 const app = express();
 app.use(cors());
